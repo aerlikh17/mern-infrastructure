7df824ebce6ea98b2caf5d4193cf9acf4d869825
aerlikh <aerlikh2011@gmail.com>
2022-11-28 10:37:21 -0500

    changes

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignUpForm from '../SignUpForm/SignUpForm'
+
+export default function AuthPage() {
+  return (
+    <main>
+    <div>AuthPage</div>
+    <SignUpForm />
+    </main>
+  );
+}
